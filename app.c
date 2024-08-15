f2c22e45279d88418bc27e3ca9b1480cf2d27db2
fizarum <fizarum@gmail.com>
2024-08-15 17:17:33 +0200

    code cleanup; (#7)

--- app.c
+++ app.c
@@ -3,98 +3,91 @@
 #include <stdlib.h>
 
 typedef struct App_t {
-	_u16 id;
-	AppSpecification_t* specification;
+  _u16 id;
+  AppSpecification_t* specification;
 
-	AppState_t state;
+  AppState_t state;
 } App_t;
 
-
 App_t* AppCreate(_u16 id, AppSpecification_t* specification) {
-	App_t* app = (App_t*)malloc(sizeof(App_t));
+  App_t* app = (App_t*)malloc(sizeof(App_t));
 
-	if (app == NULL) return NULL;
+  if (app == NULL) return NULL;
 
-	app->id = id;
-	app->specification = specification;
-	app->state = StateCreated;
+  app->id = id;
+  app->specification = specification;
+  app->state = StateCreated;
 
-	return app;
+  return app;
 }
 
 void AppDestroy(App_t* app) {
-	if (app == NULL) return;
+  if (app == NULL) return;
 
-	if (app->state != StateKilled) return;
+  if (app->state != StateKilled) return;
 
-	free(app);
+  free(app);
 }
 
 void AppOnInit(App_t* app) {
-	if (app->state != StateCreated) return;
-	app->state = StateInit;
-	app->specification->onInit();
+  if (app->state != StateCreated) return;
+  app->state = StateInit;
+  app->specification->onInit();
 }
 
 void AppOnOpen(App_t* app) {
-	if (app->state == StateInit || app->state == StateKilled) {
-		app->state = StateRunning;
-		app->specification->onStart();
-	}
+  if (app->state == StateInit || app->state == StateKilled) {
+    app->state = StateRunning;
+    app->specification->onStart();
+  }
 }
 
 void AppOnUpdate(App_t* app) {
-	if (app->state != StateRunning) return;
+  if (app->state != StateRunning) return;
 
-	app->state = StateUpdate;
-	app->specification->onUpdate();
-	app->state = StateRunning;
+  app->state = StateUpdate;
+  app->specification->onUpdate();
+  app->state = StateRunning;
 }
 
 void AppOnHandleInput(App_t* app, const void* keyData) {
-	if (app->state != StateRunning) return;
+  if (app->state != StateRunning) return;
 
-	app->state = StateUpdate;
-	app->specification->handleInput(AppGetId(app), keyData);
-	app->specification->onUpdate();
-	app->state = StateRunning;
+  app->state = StateUpdate;
+  app->specification->handleInput(AppGetId(app), keyData);
+  app->specification->onUpdate();
+  app->state = StateRunning;
 }
 
 void AppOnPause(App_t* app) {
-	if (app->state != StateRunning) return;
+  if (app->state != StateRunning) return;
 
-	app->state = StatePaused;
-	app->specification->onPause();
+  app->state = StatePaused;
+  app->specification->onPause();
 }
 
 void AppOnResume(App_t* app) {
-	if (app->state != StatePaused) return;
+  if (app->state != StatePaused) return;
 
-	app->state = StateRunning;
-	app->specification->onResume();
+  app->state = StateRunning;
+  app->specification->onResume();
 }
 
 void AppOnStop(App_t* app) {
-	if (app->state == StateRunning || app->state == StatePaused) {
-		app->state = StateStoped;
-		app->specification->onStop();
-	}
+  if (app->state == StateRunning || app->state == StatePaused) {
+    app->state = StateStoped;
+    app->specification->onStop();
+  }
 }
 
 void AppOnKill(App_t* app) {
-	if (app->state != StateStoped) return;
+  if (app->state != StateStoped) return;
 
-	app->state = StateKilled;
+  app->state = StateKilled;
 }
 
-const char* AppGetName(const App_t* app) {
-	return app->specification->name;
-}
+const char* AppGetName(const App_t* app) { return app->specification->name; }
 
-const _u16 AppGetId(const App_t* app) {
-	return app->id;
-}
+_u16 AppGetId(const App_t* app) { return app->id; }
 
-const AppState_t AppGetState(const App_t* app) {
-	return app->state;
-}
+AppState_t AppGetState(const App_t* app) { return app->state; }
--- app.h
+++ app.h
@@ -5,49 +5,49 @@
 extern "C" {
 #endif
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #include "app_state.h"
 
-	typedef uint8_t _u8;
-	typedef int8_t _i8;
-	typedef uint16_t _u16;
+typedef uint8_t _u8;
+typedef int8_t _i8;
+typedef uint16_t _u16;
 
-	typedef void (UserCallback)(const _u16 appId);
+typedef void(UserCallback)(const _u16 appId);
 
-	/** app's specific implementation */
-	typedef struct AppSpecification_t {
-		const char* name;
-		void (*handleInput)(const _u16 appId, const void* keyData);
-		void (*onInit)(void);
-		void (*onStart)(void);
-		void (*onPause)(void);
-		void (*onResume)(void);
-		void (*onUpdate)(void);
-		void (*onStop)(void);
-	} AppSpecification_t;
+/** app's specific implementation */
+typedef struct AppSpecification_t {
+  const char* name;
+  void (*handleInput)(const _u16 appId, const void* keyData);
+  void (*onInit)(void);
+  void (*onStart)(void);
+  void (*onPause)(void);
+  void (*onResume)(void);
+  void (*onUpdate)(void);
+  void (*onStop)(void);
+} AppSpecification_t;
 
-	typedef struct App_t App_t;
+typedef struct App_t App_t;
 
-	App_t* AppCreate(_u16 id, AppSpecification_t* specification);
-	void AppDestroy(App_t* app);
+App_t* AppCreate(_u16 id, AppSpecification_t* specification);
+void AppDestroy(App_t* app);
 
-	void AppOnInit(App_t* app);
-	void AppOnOpen(App_t* app);
-	void AppOnUpdate(App_t* app);
-	void AppOnHandleInput(App_t* app, const void* keyData);
-	void AppOnPause(App_t* app);
-	void AppOnResume(App_t* app);
-	void AppOnStop(App_t* app);
-	void AppOnKill(App_t* app);
+void AppOnInit(App_t* app);
+void AppOnOpen(App_t* app);
+void AppOnUpdate(App_t* app);
+void AppOnHandleInput(App_t* app, const void* keyData);
+void AppOnPause(App_t* app);
+void AppOnResume(App_t* app);
+void AppOnStop(App_t* app);
+void AppOnKill(App_t* app);
 
-	const char* AppGetName(const App_t* app);
-	const _u16 AppGetId(const App_t* app);
-	const AppState_t AppGetState(const App_t* app);
+const char* AppGetName(const App_t* app);
+_u16 AppGetId(const App_t* app);
+AppState_t AppGetState(const App_t* app);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif //APP_H
+#endif  // APP_H
