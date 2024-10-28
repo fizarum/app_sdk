e6eeb007fa7c2095b16e947271e45cbb3a24e311
fizarum <fizarum@gmail.com>
2024-10-28 18:13:46 +0100

    updated formatting; (#14)

--- broadcast/broadcast_manager.h
+++ broadcast/broadcast_manager.h
@@ -10,26 +10,26 @@ extern "C" {
 #include "../app.h"
 #include "broadcast_event.h"
 
-    typedef struct BroadcastManager_t BroadcastManager_t;
+typedef struct BroadcastManager_t BroadcastManager_t;
 
-    typedef struct BroadcastSubsriber_t {
-        BroadcastEventType_t type;
-        AppSpecification_t* spec;
-    } BroadcastSubsriber_t;
+typedef struct BroadcastSubsriber_t {
+  BroadcastEventType_t type;
+  AppSpecification_t* spec;
+} BroadcastSubsriber_t;
 
-    BroadcastManager_t* BroadcastManagerCreate();
-    BroadcastSubsriber_t* BroadcastSubsriberCreate(const BroadcastEventType_t type,
-        AppSpecification_t* spec);
+BroadcastManager_t* BroadcastManagerCreate();
+BroadcastSubsriber_t* BroadcastSubsriberCreate(const BroadcastEventType_t type,
+                                               AppSpecification_t* spec);
 
-    void BroadcastManagerUpdate(BroadcastManager_t* manager);
-    void BroadcastManagerSendEvent(BroadcastManager_t* manager,
-        BroadcastEvent_t event);
+void BroadcastManagerUpdate(BroadcastManager_t* manager);
+void BroadcastManagerSendEvent(BroadcastManager_t* manager,
+                               BroadcastEvent_t event);
 
-    bool BroadcastManagerSubscribe(const BroadcastManager_t* manager,
-        const BroadcastEventType_t eventType,
-        const App_t* app);
-    bool BroadcastManagerUnsubscribe(const BroadcastManager_t* manager,
-        const App_t* spec);
+bool BroadcastManagerSubscribe(const BroadcastManager_t* manager,
+                               const BroadcastEventType_t eventType,
+                               const App_t* app);
+bool BroadcastManagerUnsubscribe(const BroadcastManager_t* manager,
+                                 const App_t* spec);
 
 #ifdef __cplusplus
 }
