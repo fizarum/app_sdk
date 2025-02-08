a8247c7e6fbe5260c9834656d56fed47a8b84707
fizarum <fizarum@gmail.com>
2025-02-08 16:12:58 +0100

    changed BroadcastManager to singleton; added example in MenuApp for battery charge on/off event; refactored BroadcastManager API;

--- broadcast/broadcast_manager.h
+++ broadcast/broadcast_manager.h
@@ -10,26 +10,20 @@ extern "C" {
 #include "../app.h"
 #include "broadcast_event.h"
 
-typedef struct BroadcastManager_t BroadcastManager_t;
-
 typedef struct BroadcastSubsriber_t {
   BroadcastEventType_t type;
   AppSpecification_t* spec;
 } BroadcastSubsriber_t;
 
-BroadcastManager_t* BroadcastManagerCreate();
-BroadcastSubsriber_t* BroadcastSubsriberCreate(const BroadcastEventType_t type,
-                                               AppSpecification_t* spec);
+void BroadcastManager_Init();
+void BroadcastManager_Update();
 
-void BroadcastManagerUpdate(BroadcastManager_t* manager);
-void BroadcastManagerSendEvent(BroadcastManager_t* manager,
-                               BroadcastEvent_t event);
+void BroadcastManager_SendEvent(BroadcastEvent_t event);
+void BroadcastManager_SendEventType(BroadcastEventType_t eventType);
 
-bool BroadcastManagerSubscribe(const BroadcastManager_t* manager,
-                               const BroadcastEventType_t eventType,
-                               const App_t* app);
-bool BroadcastManagerUnsubscribe(const BroadcastManager_t* manager,
-                                 const App_t* spec);
+bool BroadcastManager_AddListener(const BroadcastEventType_t eventType,
+                                const App_t* app);
+bool BroadcastManager_RemoveListener(const App_t* spec);
 
 #ifdef __cplusplus
 }
